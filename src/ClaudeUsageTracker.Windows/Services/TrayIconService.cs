c18fedbfcb1b5d8b123683afae40f51b370b910d
sujayamindev <sujaya.manith23@gmail.com>
2026-07-24 11:58:38 +0530

    Revert "feat: add Claude Code Cost Stats item to the tray menu"
    This reverts commit 68979a46ffa48a258ba74dc7bdef68e9a826dab4.

--- src/ClaudeUsageTracker.Windows/Services/TrayIconService.cs
+++ src/ClaudeUsageTracker.Windows/Services/TrayIconService.cs
@@ -31,7 +31,6 @@ public sealed class TrayIconService : IDisposable
     public event EventHandler? IconStyleSettingsRequested;
     public event EventHandler? ManageProfilesRequested;
     public event EventHandler? UsageHistoryRequested;
-    public event EventHandler? CostStatsRequested;
 
     public TrayIconService(UsageViewModel viewModel, NotificationSettingsStore notificationSettingsStore, TrayIconSettingsStore trayIconSettingsStore)
     {
@@ -47,9 +46,6 @@ public sealed class TrayIconService : IDisposable
 
         var usageHistoryItem = new MenuItem { Header = "Usage History…" };
         usageHistoryItem.Click += (_, _) => UsageHistoryRequested?.Invoke(this, EventArgs.Empty);
-
-        var costStatsItem = new MenuItem { Header = "Claude Code Cost Stats…" };
-        costStatsItem.Click += (_, _) => CostStatsRequested?.Invoke(this, EventArgs.Empty);
 
         var iconStyleItem = new MenuItem { Header = "Icon Style…" };
         iconStyleItem.Click += (_, _) => IconStyleSettingsRequested?.Invoke(this, EventArgs.Empty);
@@ -69,7 +65,7 @@ public sealed class TrayIconService : IDisposable
         _taskbarIcon = new TaskbarIcon
         {
             ToolTipText = "Claude Usage Tracker",
-            ContextMenu = new ContextMenu { Items = { startupItem, manageProfilesItem, usageHistoryItem, costStatsItem, iconStyleItem, statuslineItem, notificationSettingsItem, checkForUpdatesItem, new Separator(), exitItem } }
+            ContextMenu = new ContextMenu { Items = { startupItem, manageProfilesItem, usageHistoryItem, iconStyleItem, statuslineItem, notificationSettingsItem, checkForUpdatesItem, new Separator(), exitItem } }
         };
         _taskbarIcon.TrayLeftMouseUp += (_, _) => Clicked?.Invoke(this, EventArgs.Empty);
         _taskbarIcon.TrayBalloonTipClicked += (_, _) => UpdateNotificationClicked?.Invoke(this, EventArgs.Empty);
